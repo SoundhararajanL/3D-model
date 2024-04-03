2d4e1521bc3a44dbd0cd0d72fd6d1f8978ae6c26
SoundhararajanL <sowndhar00001@gmail.com>
2024-04-03 16:22:38 +0530

    make some css changes

--- src/components/loginpage.js
+++ src/components/loginpage.js
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
 import axios from 'axios';
 import Login3d from "./login";
+import { ToastContainer, toast, Zoom } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 function Login() {
   const [users, setUsers] = useState({
@@ -20,50 +22,99 @@ function Login() {
     e.preventDefault();
     try {
       await axios.post('http://localhost:3000/users/login', users);
-      alert('Data submitted successfully!');
+      toast('💻 Login Successfully !', {
+        position: "top-center",
+        autoClose: 800,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "light",
+        transition: Zoom,
+      });
       setUsers({ username: '', password: '' });
     } catch (error) {
       console.error('Error submitting data:', error);
     }
   };
-  
+
   return (
     <div className="login-container">
       <h2 className="mt-5">Login</h2>
       <div className="split-container">
         <div className="left-half">
-          <Login3d/>
+          <Login3d />
         </div>
         <div className="right-half">
-          <form onSubmit={handleSubmit} className="login-form mt-3">
-            <div className="form-group">
-              <label htmlFor="username">Username:</label>
-              <input
-                type="text"
-                className="form-control"
-                id="username"
-                name="username"
-                value={users.username}
-                onChange={handleChange}
-                required
-              />
+          <div class="card">
+            <div class="tools">
+              <div class="circle">
+                <span class="red box"></span>
+              </div>
+              <div class="circle">
+                <span class="yellow box"></span>
+              </div>
+              <div class="circle">
+                <span class="green box"></span>
+              </div>
             </div>
-            <div className="form-group">
-              <label htmlFor="password">Password:</label>
-              <input
-                type="password"
-                className="form-control"
-                id="password"
-                name="password"
-                value={users.password}
-                onChange={handleChange}
-                required
-              />
+            <div class="card__content">
+              <form onSubmit={handleSubmit} className="login-form mt-3">
+                <div className="form-group">
+                  <div class="form-control">
+                    <input
+                      type="text"
+                      id="username"
+                      name="username"
+                      value={users.username}
+                      onChange={handleChange}
+                      required
+                    />
+                    <label>
+                      <span style={{ transitionDelay: '0ms' }}>U</span>
+                      <span style={{ transitionDelay: '50ms' }}>s</span>
+                      <span style={{ transitionDelay: '100ms' }}>e</span>
+                      <span style={{ transitionDelay: '150ms' }}>r</span>
+                      <span style={{ transitionDelay: '200ms' }}>n</span>
+                      <span style={{ transitionDelay: '250ms' }}>a</span>
+                      <span style={{ transitionDelay: '300ms' }}>m</span>
+                      <span style={{ transitionDelay: '350ms' }}>e</span>
+                    </label>
+                  </div>
+                </div>
+                <div className="form-group">
+                  <div class="form-control">
+                    <input
+                      type="password"
+                      id="password"
+                      name="password"
+                      value={users.password}
+                      onChange={handleChange}
+                      required
+                    />
+                    <label>
+                      <span style={{ transitionDelay: '0ms' }}>P</span>
+                      <span style={{ transitionDelay: '50ms' }}>a</span>
+                      <span style={{ transitionDelay: '100ms' }}>s</span>
+                      <span style={{ transitionDelay: '150ms' }}>s</span>
+                      <span style={{ transitionDelay: '200ms' }}>w</span>
+                      <span style={{ transitionDelay: '250ms' }}>o</span>
+                      <span style={{ transitionDelay: '300ms' }}>r</span>
+                      <span style={{ transitionDelay: '350ms' }}>d</span>
+                    </label>
+                  </div>
+                </div>
+                <button className="btn-23">
+                  <span className="text">Login</span>
+                  <span aria-hidden="" className="marquee">Login</span>
+                </button>
+              </form>
             </div>
-            <button type="submit" className="btn btn-primary">Login</button>
-          </form>
+          </div>
         </div>
       </div>
+      <ToastContainer />
     </div>
   );
 }
